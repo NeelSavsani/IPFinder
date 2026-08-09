15109255526ce0b75cdeeb42b63eaf20ed48e11b
NeelSavsani <neelsavsani7@gmail.com>
2026-08-10 01:01:11 +0530

    Update live IP geolocation mini-map theme to bright white/light mode

--- script.js
+++ script.js
@@ -859,10 +859,9 @@ function renderIpMap(lat, lon, city, region, country) {
       scrollWheelZoom: false
     });
 
-    // Dark Matter CartoDB tiles for dark glassmorphism aesthetic matching
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>',
-      subdomains: 'abcd',
+    // Standard Bright White / Light Map Tile Layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       maxZoom: 19
     }).addTo(mapInstance);
   } else {
